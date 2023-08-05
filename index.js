778a8caa07337d9f516c6f98c394facb014ab3c9
Rahul-Sahani04 <devilken43@gmail.com>
2023-08-05 13:11:27 +0530

    bleep

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
 // app.use('/', app);
 
 const search = require('./controllers/search')

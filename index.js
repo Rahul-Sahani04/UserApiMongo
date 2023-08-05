cd867090544e704ccf785808b3fae4db9dda0eb3
Rahul-Sahani04 <devilken43@gmail.com>
2023-08-05 13:10:47 +0530

    Major changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./routes/main_routes');
+// const router = require('./routes/main_routes');
 
 const mongoose = require('mongoose');
 
@@ -12,8 +12,28 @@ const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
+// app.use('/', app);
 
-app.use('/', router); // Use the imported router as middleware
+const search = require('./controllers/search')
+const users = require('./controllers/ShowData')
+const add = require('./controllers/add')
+const deleteUser = require('./controllers/delete')
+const update = require('./controllers/update')
+
+const auth = require('./controllers/auth')
+
+app.get('/', (request, response) => {
+    response.json({
+        info: 'Node.js, Express, and Postgres API'
+    })
+})
+
+app.post('/users/auth', auth.authUser)
+app.get('/users', users.getUsers)
+app.get('/users/search', search.searchUser)
+app.post('/users/add', add.addUsers)
+app.post('/users/:id', update.updateUser)
+app.delete('/users/:id', deleteUser.deleteUser)
 
 mongoose.connect(URI)
   .then(() => {

b1df8f8b0437b2faf283b13a445536f69ce43bcb
Rahul-Sahani04 <devilken43@gmail.com>
2023-08-05 20:52:17 +0530

    update index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
-// const APIroutes = require('./routes/routes') 
-// const mongo = require('mongoose')
+const { getUsers } = require('./controllers/ShowData')
+const { addUsers } = require('./controllers/add')
+const { authUser } = require('./controllers/auth')
+const { deleteUser } = require('./controllers/delete')
+const { searchUser } = require('./controllers/search')
+const { updateUser } = require('./controllers/update')
 
 const app = express();
 const cors = require("cors")
@@ -13,6 +17,15 @@ const port = process.env.PORT || 8000
 app.get("/", (req, res) => {
   res.json({ page: "Main page!", info: 'Node.js, Express, and Postgres API', availableRoutes: ["/cookie/all", "/cookie/random", "/cookie/add"] });
 });
+
+app.get("/users/all", (req, res) => {
+  res.json({ users: "ALL" });
+});
+
+app.get("/users/search", (req, res) => {
+  res.json({ users: "Searching" });
+});
+
 
 // app.use("/api", APIroutes)
 

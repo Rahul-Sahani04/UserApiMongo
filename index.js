ed659f17982cc682fc66da0f5d1022a644031fd2
Rahul-Sahani04 <devilken43@gmail.com>
2023-08-05 20:04:21 +0530

    Major Changes

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const { addUsers } = require('./controllers/add')
+const { deleteUser } = require('./controllers/delete')
+const { searchUser } = require('./controllers/search')
+const { getUsers } = require('./controllers/ShowData')
+const { updateUser } = require('./controllers/update')
+const { authUser } = require('./controllers/auth')
+
+
 const app = express();
 const cors = require("cors")
 app.use(cors())
 require("dotenv").config();
-const PORT = process.env.PORT || 8000
-const URI = process.env.MONGO_URI
+const port = process.env.PORT || 8000
 
 
 app.get("/", (req, res) => {
-    res.json({ page: "Main page!", info: 'Node.js, Express, and Postgres API', availableRoutes: ["/cookie/all", "/cookie/random", "/cookie/add"] });
+  res.json({ page: "Main page!", info: 'Node.js, Express, and Postgres API', availablecontrollers: ["/users/all", "/users/random", "/users/add"] });
 });
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}.`);
+app.get('/users/all', getUsers)
+app.get('/users/add', addUsers)
+app.post('/users/search', searchUser)
+app.delete('/users/:id', deleteUser)
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
+
+module.exports = { app };
